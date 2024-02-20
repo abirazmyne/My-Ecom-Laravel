bf90d27f07e783986a5aacde5f2bedab6c410277
abirazmyne <abirazmyne0011@gmail.com>
2024-02-21 03:20:48 +0600

    Title Update

--- resources/views/website/master.blade.php
+++ resources/views/website/master.blade.php
@@ -6,7 +6,7 @@
     <title>Shop-Boneek.</title>
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
-    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/')}}website/assets/images/favicon.svg" />
+    <link rel="shortcut icon" type="image/x-icon" href="{{asset('/')}}website/assets/images/logome.png" />
 
     <link rel="stylesheet" href="{{asset('/')}}website/assets/css/bootstrap.min.css" />
     <link rel="stylesheet" href="{{asset('/')}}website/assets/css/LineIcons.3.0.css" />
